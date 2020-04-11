3b5e7a4767f6aa91c924883792cfe71e17963690
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-04-11 14:00:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,15 +2,15 @@ from flask import Flask
 server = Flask(__name__)
 @server.route('/')
 def index():
-	string = '<title>Home</title><style>body{width:100%;height:100%;background:url(https://im0-tub-ru.yandex.net/i?id=5fb8f760eb245cdc54d49de2d7c811ec&n=13);}</style><h1>Home Page</h1><div style="display:flex;justify-content:space-around"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
+	string = '<title>Home</title><style>body{width:100%;height:100%;background:green;}</style><h1>Home Page</h1><div style="display:flex;justify-content:space-around"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
 	return(string)
 @server.route('/app')
 def app():
-	string = '<title>App</title><style>body{background:url(https://im0-tub-ru.yandex.net/i?id=5fb8f760eb245cdc54d49de2d7c811ec&n=13);}</style><h1>App</h1><div style="display:flex;justify-content:space-around"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
+	string = '<title>App</title><style>body{background:green;}</style><h1>App</h1><div style="display:flex;justify-content:space-around"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
 	return(string)
 @server.route('/help')
 def help():
-	string = '<title>Help</title><style>body{background:url(https://im0-tub-ru.yandex.net/i?id=5fb8f760eb245cdc54d49de2d7c811ec&n=13);}</style><h1>Help Page</h1><div style="display:flex;justify-content:space-around;"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
+	string = '<title>Help</title><style>body{background:green;}</style><h1>Help Page</h1><div style="display:flex;justify-content:space-around;"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
 	return (string)
 print('Do you have a root?')
 print('1 -yes')
@@ -18,9 +18,9 @@ print('2-no')
 roots = int(input('1/2'))
 if (roots == 1):
 	if __name__=="__main__":
-		app.run(host='0.0.0.0', port=80)
+		server.run(host='0.0.0.0', port=80)
 else:
 	
 	if __name__=="__main__":
-		app.run()
+		server.run()
 
