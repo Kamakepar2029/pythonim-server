aed4f8ece16b055ab5ace06ca10171a8dfbe83f2
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-04-11 10:00:32 +0000

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,15 @@ def app():
 def help():
 	string = '<title>Help</title><style>body{background:url(https://im0-tub-ru.yandex.net/i?id=5fb8f760eb245cdc54d49de2d7c811ec&n=13);}</style><h1>Help Page</h1><div style="display:flex;justify-content:space-around;"><a href="/">Home</a><br><a href="/app">App</a><br><a href="/help">Help Page</a><br></div>'
 	return (string)
-if __name__=="__main__":
-	server.run()
+print('Do you have a root?')
+print('1 -yes')
+print('2-no')
+roots = int(input('1/2'))
+if (roots == 1):
+	if __name__=="__main__":
+		app.run(host='0.0.0.0', port=80)
+else:
+	
+	if __name__=="__main__":
+		app.run()
 
